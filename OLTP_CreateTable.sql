9a19da8be2e68e1722edd5adad920eb59a576d8e
songlingg <lisongling.21@gmail.com>
2022-07-13 21:44:12 +0800

    OLTP sql

--- OLTP_CreateTable.sql
+++ OLTP_CreateTable.sql
@@ -41,14 +41,14 @@ CREATE TABLE production.categories (
 CREATE TABLE production.brands ( 
   brand_id varchar(5) PRIMARY KEY,
   brand_name VARCHAR (255) NOT NULL);
-  GO
+
 CREATE TABLE production.products (
   product_id varchar(10) PRIMARY KEY,  
   product_name VARCHAR (255) NOT NULL,  
   brand_id varchar(5) NOT NULL,  
   category_id varchar(5) NOT NULL,  
   model_year int NOT NULL, 
-  list_price DECIMAL (10, 2) NOT NULL,  
+  list_price DECIMAL(10, 2) NOT NULL,  
   FOREIGN KEY (category_id) 
   REFERENCES production.categories (category_id)   
   ON DELETE CASCADE 
@@ -57,7 +57,7 @@ CREATE TABLE production.products (
   REFERENCES production.brands (brand_id)   
   ON DELETE CASCADE 
   ON UPDATE CASCADE );
-  GO
+
 CREATE TABLE sales.customers (  
   customer_id varchar(10) PRIMARY KEY,  
   first_name VARCHAR (255) NOT NULL,  
--- OLTP_InsertTable.sql
+++ OLTP_InsertTable.sql
@@ -1,2 +1,46 @@
 USE [BikeSalesGroup4];
 GO
+
+-- Customer Table (Joaquin)
+
+
+-- Orders Table (Joaquin)
+
+
+-- Order items Table (Joaquin)
+
+
+-- Stocks Table (Song Ling)
+ALTER TABLE production.stocks NOCHECK CONSTRAINT ALL
+BULK INSERT production.stocks
+FROM 'C:\DAAA\Y2S1\DENG_CA2\Stocks.csv'
+WITH (fieldterminator=',', rowterminator='\n')
+
+-- Products Table (Song Ling)
+ALTER TABLE production.products NOCHECK CONSTRAINT ALL
+Declare @Products VARCHAR(max) 
+
+SELECT @Products =  
+  BulkColumn 
+    FROM OPENROWSET(BULK 'C:\DAAA\Y2S1\DENG_CA2\products.json', SINGLE_BLOB) JSON 
+
+INSERT INTO production.products 
+  SELECT * FROM OpenJSON(@Products, '$')  
+  WITH ( 
+  product_id VARCHAR(10) '$.product_id', 
+  product_name VARCHAR (255) '$.product_name', 
+  brand_id VARCHAR(5) '$.brand_id', 
+  category_id VARCHAR(5) '$.category_id', 
+  model_year INT '$.model_year', 
+  list_price DECIMAL(10, 2) '$.list_price')
+
+-- Stores Table (Rachel)
+
+
+-- Staff Table (Rachel)
+
+
+-- Brand Table (Cody)
+
+
+-- Categories Table (Cody)
