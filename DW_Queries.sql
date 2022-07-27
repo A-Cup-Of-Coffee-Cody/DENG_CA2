891e9247485475329a8982609e9bae72df9180f0
songlingg <lisongling.21@gmail.com>
2022-07-27 12:47:02 +0800

    Update Queries

--- DW_Queries.sql
+++ DW_Queries.sql
@@ -5,13 +5,13 @@ GO
   -- - Find for:
   --     - sales
   --     - qty
-  -- - Group by:
+  -- - Group BY:
   --     - items
   --     - discounts
-  -- - Order by:
+  -- - Order BY:
   --     - Best to worst total sales generated
 
-SELECT f.product_id, p.product_name AS 'Product Name', ROUND(SUM(f.sales), 2) AS 'Total Sales', ROUND(SUM(f.profit), 2) AS 'Total Profit', SUM(f.quantity) as Quantity, CONCAT(CAST(AVG(f.discount)*100 AS INT),'%') as 'Average Discount'
+SELECT f.product_id, p.product_name AS 'Product Name', ROUND(SUM(f.sales), 2) AS 'Total Sales', ROUND(SUM(f.profit), 2) AS 'Total Profit', SUM(f.quantity) AS Quantity, CONCAT(CAST(AVG(f.discount)*100 AS INT),'%') AS 'Average Discount'
 FROM factTable f, productDim p
 WHERE f.product_id = p.product_id
 GROUP BY f.product_id, p.product_name
@@ -20,20 +20,20 @@ ORDER BY [Total Profit] DESC;
 -- Sales/Staff/stores (Joaquin)
   -- - Find for:
   --     - staff that makes most money
-  -- - Order by:
+  -- - Order BY:
   --     - sales
 
 SELECT (s.first_name + ' '+ s.last_name) 'Staff Name', ROUND(SUM(f.sales * f.discount), 2) 'Total Sales', f.store_id 'Store Code'
 FROM factTable f, staffDim s
 WHERE f.staff_id = s.staff_id
 GROUP BY (s.first_name + ' '+ s.last_name), f.store_id
-ORDER BY [Total Sales] DESC
--- From here I can infer there are only 6 active staffs out of the 9, out of thesee 6 staffs, ST2 staffs have the most sales, followed by ST1 and finally ST3.
+ORDER BY [Total Sales] DESC;
+-- From here I can infer there are only 6 active staffs out of the 9, out of thesee 6 staffs, ST2 staffs have the most sales, followed BY ST1 and finally ST3.
 
 -- Sales/Seasons of Sales/time (Rachel)
   -- - Find for:
   --     - Seasons of Sales (Weekly)
-  -- - Order by:
+  -- - Order BY:
   --     - Sales
 
 SELECT SUM(f.sales) AS 'Total Sales',t.Year, t.WeekOfYear AS 'Week Of Year', t.MonthName AS 'Month'
@@ -46,7 +46,7 @@ ORDER BY [Total Sales] DESC, t.Year, CAST(t.WeekOfYear AS INT)
   -- - Find for:
   --     - customer sales
   --     - qty of orders
-  -- - Order by:
+  -- - Order BY:
   --     - state
   --     - city
 SELECT (c.first_name + ' '+ c.last_name) 'Customer Name', ROUND(SUM(f.sales * f.discount), 2) 'Total Sales', COUNT(f.order_id) 'Quantity of Items Bought', c.[state] 'State', c.city 'City'
@@ -60,12 +60,13 @@ ORDER BY [Total Sales] DESC, c.[state], c.city
   --     - Categories
   --     - Brands
   --     - Products
-  -- - Order by:
+  -- - Order BY:
   --     - sales / tc (show inventory)
 
--- compares sales over quantity sold and stock available
-SELECT c.category_name AS 'Category Name', b.brand_name AS 'Brand Name', p.product_name AS 'Product Name', SUM(f.quantity) as 'Quantity Sold', p.stock AS Stock, ROUND(SUM(f.sales), 2) AS 'Total Sales', ROUND(SUM(f.profit), 2) AS 'Total Profit'
+SELECT [Category Name], [Brand Name], [Product Name], [Quantity Sold], Stock, [Total Sales], [Total Profit] FROM
+(SELECT c.category_name AS 'Category Name', b.brand_name AS 'Brand Name', p.product_name AS 'Product Name', SUM(f.quantity) AS 'Quantity Sold', p.stock AS Stock, ROUND(SUM(f.sales), 2) AS 'Total Sales', ROUND(SUM(f.profit), 2) AS 'Total Profit', RANK() OVER (PARTITION BY c.category_name ORDER BY ROUND(SUM(f.sales), 2) DESC) AS [Ranking]
 FROM factTable f, categoryDim c, brandDim b, productDim p
 WHERE f.product_id = p.product_id AND p.category_id = c.category_id AND p.brand_id = b.brand_id
-GROUP BY c.category_name, b.brand_name, p.product_name, p.stock
-ORDER BY [Total Sales] DESC, [Quantity Sold] ASC, [Stock] DESC;
+GROUP BY c.category_name, b.brand_name, p.product_name, p.stock) AS ranktable
+WHERE ranking = 1
+ORDER BY [Total Sales] DESC
